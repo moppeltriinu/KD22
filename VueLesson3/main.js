9f1ab6f3d7735bc547373bb69113929bab12113d
moppeltriinu <113966310+moppeltriinu@users.noreply.github.com>
2022-12-12 11:27:30 +0200

    Update main.js

--- VueLesson3/main.js
+++ VueLesson3/main.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg',
+            image: './assets/socks_green.jpg',
             // solution
             url: 'https://www.vuemastery.com/'
             // solution
