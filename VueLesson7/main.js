8d35d23da563006480dbebbed34736a5ca163154
moppeltriinu <113966310+moppeltriinu@users.noreply.github.com>
2022-12-12 11:35:25 +0200

    Create main.js

---
+++ VueLesson7/main.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            cart:0,
+            product: 'Socks',
+            brand: 'Vue Mastery',
+            image: './assets/socks_blue.jpg',
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green', image: './assets/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/socks_blue.jpg' },
+            ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        }
+    }
+})
